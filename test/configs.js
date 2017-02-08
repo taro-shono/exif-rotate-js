283f9761e297c500d2e3d483b3e78b9cca054bb6
hanagejet <taroage@gmail.com>
2017-02-08 17:00:54 +0900

    Update to split method

--- test/configs.js
+++ test/configs.js
@@ -1,8 +1,10 @@
 import test from 'ava';
 import configs from '../src/configs';
 
-test('test configs', async t => {
+test('Should be number of max_size', async t => {
   t.is(await typeof configs.max_size, 'number');
+});
 
+test('Should be string of default_container_id', async t => {
   t.is(await typeof configs.default_container_id, 'string');
 });
