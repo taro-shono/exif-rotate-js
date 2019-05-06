aba8e1aab74855beb361ef1002a6739cb1843742
hanagejet <taroage@gmail.com>
2019-05-06 22:19:02 +0900

    chore: use typescript

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+  },
+};
