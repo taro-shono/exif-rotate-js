8e3a6b7206e23d4bf0e773f1126e17f6e7d1c886
hanagejet <taroage@gmail.com>
2020-01-13 23:30:41 +0900

    feat: update logic of getSize()
    - when masSize is higher than image size, use default size

--- src/utils/__tests__/getImages.test.js
+++ src/utils/__tests__/getImages.test.js
@@ -2,11 +2,23 @@ import { getSize } from '../getImages';
 import { defaultOptions } from '../../options';
 
 const MAX_SIZE = 200;
-const GREATERE_WIDTH = {
+const GREATER_MAXSIZE_WIDTH = {
+  width: 3000,
+  height: 10,
+};
+const GREATER_MAXSIZE_HEIGHT = {
+  width: 10,
+  height: 3000,
+};
+const GREATER_MAXSIZE_SAME_WIDTH_HEIGHT = {
+  width: 3000,
+  height: 3000,
+};
+const GREATER_WIDTH = {
   width: 100,
   height: 10,
 };
-const GREATERE_HEIGHT = {
+const GREATER_HEIGHT = {
   width: 10,
   height: 100,
 };
@@ -17,7 +29,7 @@ const SAME_WIDTH_HEIGHT = {
 
 describe('getSize()', () => {
   it('Should be return MAX_SIZE of width', () => {
-    const { width, height } = GREATERE_WIDTH;
+    const { width, height } = GREATER_MAXSIZE_WIDTH;
     expect(getSize(width, height, MAX_SIZE)).toEqual({
       width: MAX_SIZE,
       height: height * (MAX_SIZE / width),
@@ -25,23 +37,55 @@ describe('getSize()', () => {
   });
 
   it('Should be return MAX_SIZE of height', () => {
-    const { width, height } = GREATERE_HEIGHT;
+    const { width, height } = GREATER_MAXSIZE_HEIGHT;
     expect(getSize(width, height, MAX_SIZE)).toEqual({
       width: width * (MAX_SIZE / height),
       height: MAX_SIZE,
     });
   });
 
-  it('Should be return MAX_SIZE of width and height', () => {
+  it('Should be return default size, when width is lower size than MAX_SIZE', () => {
+    const { width, height } = GREATER_WIDTH;
+    expect(getSize(width, height, MAX_SIZE)).toEqual({
+      width,
+      height,
+    });
+  });
+
+  it('Should be return default size, when height is lower size than MAX_SIZE', () => {
+    const { width, height } = GREATER_HEIGHT;
+    expect(getSize(width, height, MAX_SIZE)).toEqual({
+      width,
+      height,
+    });
+  });
+
+  it('Should be return of default size, when width and height are lower size than MAX_SIZE', () => {
     const { width, height } = SAME_WIDTH_HEIGHT;
+    expect(getSize(width, height, MAX_SIZE)).toEqual({
+      width,
+      height,
+    });
+  });
+
+  it('Should be return MAX_SIZE of width and height', () => {
+    const { width, height } = GREATER_MAXSIZE_SAME_WIDTH_HEIGHT;
     expect(getSize(width, height, MAX_SIZE)).toEqual({
       width: MAX_SIZE,
       height: MAX_SIZE,
     });
   });
 
-  it('Should be return width, height without argument of MAX_SIZE', () => {
-    const { width, height } = GREATERE_WIDTH;
+  it('Should be return default width and height without argument of MAX_SIZE', () => {
+    const { width, height } = GREATER_WIDTH;
+    expect(getSize(width, height)).toEqual({
+      width,
+      height,
+    });
+  });
+
+  it('Should be return default MAX_SIZE without argument of MAX_SIZE', () => {
+    const { width, height } = GREATER_MAXSIZE_WIDTH;
     const { maxSize } = defaultOptions;
     expect(getSize(width, height)).toEqual({
       width: maxSize,
